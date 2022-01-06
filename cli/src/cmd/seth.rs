78de0a61bd8896e18cef4e684c883af2513c2524
echo <hujw77@gmail.com>
2022-01-06 13:05:49 +0800

    seth verify two calldata

--- cli/src/cmd/seth.rs
+++ cli/src/cmd/seth.rs
@@ -14,6 +14,48 @@ pub enum Cast {
         sig: String,
         args: Vec<String>,
     },
+    #[structopt(name = "--calldata-decode")]
+    #[structopt(
+        about = "Decode ABI-encoded hex input data. Use `--abi-decode` to decode output data"
+    )]
+    CalldataDecode {
+        #[structopt(
+            help = "the function signature you want to decode, in the format `<name>(<in-types>)(<out-types>)`"
+        )]
+        sig: String,
+        #[structopt(help = "the encoded calladata, in hex format")]
+        calldata: String,
+    },
+    #[structopt(name = "--abi-decode")]
+    #[structopt(
+        about = "Decode ABI-encoded hex output data. Pass --input to decode as input, or use `--calldata-decode`"
+    )]
+    AbiDecode {
+        #[structopt(
+            help = "the function signature you want to decode, in the format `<name>(<in-types>)(<out-types>)`"
+        )]
+        sig: String,
+        #[structopt(help = "the encoded calladata, in hex format")]
+        calldata: String,
+        #[structopt(long, short, help = "the encoded output, in hex format")]
+        input: bool,
+    },
+    #[structopt(name = "abi-encode")]
+    #[structopt(
+        help = "ABI encodes the given arguments with the function signature, excluidng the selector"
+    )]
+    AbiEncode {
+        #[structopt(help = "the function signature")]
+        sig: String,
+        #[structopt(help = "the list of function arguments")]
+        args: Vec<String>,
+    },
+    #[structopt(name = "keccak")]
+    #[structopt(about = "Keccak-256 hashes arbitrary data")]
+    Keccak { data: String },
+    #[structopt(name = "verify")]
+    #[structopt(about = "verify two calldata is equel")]
+    Verify { first: String, second: String },
 }
 
 impl Cast {
@@ -21,6 +63,31 @@ impl Cast {
         match self {
             Cast::Calldata { sig, args } => {
                 println!("{}", SimpleCast::calldata(sig, &args)?);
+            }
+            Cast::CalldataDecode { sig, calldata } => {
+                let tokens = SimpleCast::abi_decode(&sig, &calldata, true)?;
+                let tokens = foundry_utils::format_tokens(&tokens);
+                tokens.for_each(|t| println!("{}", t));
+            }
+            Cast::AbiDecode {
+                sig,
+                calldata,
+                input,
+            } => {
+                let tokens = SimpleCast::abi_decode(&sig, &calldata, input)?;
+                let tokens = foundry_utils::format_tokens(&tokens);
+                tokens.for_each(|t| println!("{}", t));
+            }
+            Cast::AbiEncode { sig, args } => {
+                println!("{}", SimpleCast::abi_encode(&sig, &args)?);
+            }
+            Cast::Keccak { data } => {
+                println!("{}", SimpleCast::keccak(&data)?);
+            }
+            Cast::Verify { first, second } => {
+                let _first = SimpleCast::keccak(&first)?;
+                let _second = SimpleCast::keccak(&second)?;
+                println!("{:?}", _first.cmp(&_second));
             }
         }
         Ok(())
