5e353cec54b92d56ea6229e8dfe463be6f8badf6
echo <hujw77@gmail.com>
2022-01-13 12:37:15 +0800

    shell.nix

--- shell.nix
+++ shell.nix
@@ -1,10 +1,14 @@
-{ profile ? "default" }:
-with import <nixpkgs> {
-  overlays = [ (import <rust-overlay>) ];
-};
-mkShell {
+let
+  rust-overlay =
+    import (builtins.fetchGit {
+      url = "https://github.com/oxalica/rust-overlay.git";
+      rev = "84c58400556c1c5fa796cbc3215ba5bbd3bd848f";
+    });
+  nixpkgs = import <nixpkgs> { overlays = [ rust-overlay ]; };
+in
+with nixpkgs; pkgs.mkShell {
   nativeBuildInputs = [
-    rust-bin.stable.latest.${profile}
+    rust-bin.stable.latest.minimal
     pkg-config
   ];
 
